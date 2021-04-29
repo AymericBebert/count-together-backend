825dfd30c724f59f95f48343dc0675114e4fa19c
AymericBebert <aymeric.bernard@hotmail.fr>
2021-04-29 20:52:41 +0200

    Build script update

--- build-image.sh
+++ build-image.sh
@@ -2,16 +2,14 @@
 
 BUILD_CONFIGURATION=$1
 
-if [ -z $BUILD_CONFIGURATION ]
-then
+if [ -z $BUILD_CONFIGURATION ]; then
   read -p "Enter build configuration [PRODUCTION/testing]: " bc
   BUILD_CONFIGURATION=${bc:-production}
 fi
 
-tags=`git describe --contains`
+tags=$(git describe --contains)
 
-if [ -z $tags ]
-then
+if [ -z $tags ]; then
   echo "No tag on current commit. Latest tags:"
   git tag | sort -V | tail -n 3
 
@@ -19,18 +17,15 @@ then
 
   [ -z "$newtag" ] && echo "No version specified" && exit 1
 
-  if ! git tag $newtag
-  then
+  if ! git tag $newtag; then
     read -p "Git tag failed, continue? [y/N]: " c
-    if [[ ! $c =~ ^[Yy]$ ]]
-    then
+    if [[ ! $c =~ ^[Yy]$ ]]; then
       echo "Cancelled"
       exit 2
     fi
   fi
 else
-  if [[ $tags == *" "* ]]
-  then
+  if [[ $tags == *" "* ]]; then
     echo "Ambiguous tag. Aborting."
     exit 1
   else
@@ -40,23 +35,31 @@ else
 fi
 
 version=$newtag
-if [ "$BUILD_CONFIGURATION" != "production" ]
-then
+if [ "$BUILD_CONFIGURATION" != "production" ]; then
   version=$newtag-$BUILD_CONFIGURATION
 fi
 
 read -p "Will build version $version, configuration $BUILD_CONFIGURATION, continue? [y/N]: " c
-if [[ ! $c =~ ^[Yy]$ ]]
-then
+if [[ ! $c =~ ^[Yy]$ ]]; then
   echo "Cancelled"
   exit 2
 fi
 
+echo "-----"
 echo "Building count-together-backend:$version..."
-docker build -t aymericbernard/count-together-backend:$version --build-arg BUILD_CONFIGURATION=$BUILD_CONFIGURATION --build-arg VERSION=$version . || { echo 'build failed' ; exit 1; }
+docker build -t aymericbernard/count-together-backend:$version --build-arg BUILD_CONFIGURATION=$BUILD_CONFIGURATION --build-arg VERSION=$version . ||
+  {
+    echo 'Build failed'
+    delete_new_tag
+    exit 1
+  }
 
 echo "Pushing count-together-backend:$version to docker registry..."
-docker push aymericbernard/count-together-backend:$version || { echo 'push failed' ; exit 1; }
+docker push aymericbernard/count-together-backend:$version ||
+  {
+    echo 'Push failed'
+    exit 1
+  }
 
-echo "Pushing git tags..."
-git push --tags
+echo "Pushing git tag..."
+git push origin $version
