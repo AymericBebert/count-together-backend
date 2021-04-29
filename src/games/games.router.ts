1db16e36d5de80d86e3c84e569e88936aebaf70f
AymericBebert <aymeric.bernard@hotmail.fr>
2021-04-29 21:34:18 +0200

    Duplicate game route

--- src/games/games.router.ts
+++ src/games/games.router.ts
@@ -13,10 +13,6 @@ import {
 } from '../model/game';
 import {generateToken} from '../utils/generate-token';
 
-interface ParamsDictionary {
-    [key: string]: string;
-}
-
 interface WithError<T> {
     result: T | null;
     error: string;
@@ -24,12 +20,29 @@ interface WithError<T> {
 
 const router = Router();
 
-router.post<ParamsDictionary, WithError<IGame>, IGame>('/new-game', (request, response) => {
+router.post<{}, WithError<IGame>, IGame>('/new-game', (request, response) => {
     const gameId = generateToken(8);
     GamesService.addGame({...request.body, gameId})
         .then(game => response.send({result: game, error: ''}))
         .catch(err => {
             console.warn('Error in POST /games/new-game', err);
+            response.status(500).send({result: null, error: err.toString()});
+        });
+});
+
+router.post<{ gameId: string }, WithError<IGame>>('/duplicate/:gameId', (request, response) => {
+    const gameId = request.params.gameId;
+    const newGameId = generateToken(8);
+    GamesService.getGameById(gameId)
+        .then(game => {
+            if (!game) {
+                throw new Error('Cannot copy: did not find game');
+            }
+            return GamesService.addGame({...game, gameId: newGameId, name: game.name + ' - copy'});
+        })
+        .then(game => response.send({result: game, error: ''}))
+        .catch(err => {
+            console.warn(`Error in POST /duplicate/${gameId}`, err);
             response.status(500).send({result: null, error: err.toString()});
         });
 });
@@ -44,14 +57,14 @@ router.get<{ gameId: string }, WithError<IGame>>('/game/:gameId', (request, resp
         });
 });
 
-router.get<ParamsDictionary, WithError<IGame[]>>('/games', (request, response) => {
-    GamesService.getGames()
-        .then(games => response.send({result: games, error: ''}))
-        .catch(err => {
-            console.warn('Error in GET /games/games', err);
-            response.status(500).send({result: [], error: err.toString()});
-        });
-});
+// router.get<{}, WithError<IGame[]>>('/games', (request, response) => {
+//     GamesService.getGames()
+//         .then(games => response.send({result: games, error: ''}))
+//         .catch(err => {
+//             console.warn('Error in GET /games/games', err);
+//             response.status(500).send({result: [], error: err.toString()});
+//         });
+// });
 
 router.put<{ gameId: string }, WithError<IGame>, IGame>('/game/:gameId', (request, response) => {
     const gameId = request.params.gameId;
@@ -78,7 +91,7 @@ router.delete<{ gameId: string }, WithError<null>>('/game/:gameId', (request, re
         });
 });
 
-router.post<ParamsDictionary, WithError<IGame>, IGameEditName>('/game-edit/name', (request, response) => {
+router.post<{}, WithError<IGame>, IGameEditName>('/game-edit/name', (request, response) => {
     const edit = request.body;
     GamesService.updateGameName(edit.gameId, edit.name)
         .then(game => response.send({result: game, error: ''}))
@@ -88,7 +101,7 @@ router.post<ParamsDictionary, WithError<IGame>, IGameEditName>('/game-edit/name'
         });
 });
 
-router.post<ParamsDictionary, WithError<IGame>, IGameEditGameType>('/game-edit/type', (request, response) => {
+router.post<{}, WithError<IGame>, IGameEditGameType>('/game-edit/type', (request, response) => {
     const edit = request.body;
     GamesService.updateGameType(edit.gameId, edit.gameType)
         .then(game => response.send({result: game, error: ''}))
@@ -98,7 +111,7 @@ router.post<ParamsDictionary, WithError<IGame>, IGameEditGameType>('/game-edit/t
         });
 });
 
-router.post<ParamsDictionary, WithError<IGame>, IGameEditWin>('/game-edit/win', (request, response) => {
+router.post<{}, WithError<IGame>, IGameEditWin>('/game-edit/win', (request, response) => {
     const edit = request.body;
     GamesService.updateGameWin(edit.gameId, edit.lowerScoreWins)
         .then(game => response.send({result: game, error: ''}))
@@ -108,7 +121,7 @@ router.post<ParamsDictionary, WithError<IGame>, IGameEditWin>('/game-edit/win', 
         });
 });
 
-router.post<ParamsDictionary, WithError<IGame>, IGameEditPlayer>('/game-edit/player', (request, response) => {
+router.post<{}, WithError<IGame>, IGameEditPlayer>('/game-edit/player', (request, response) => {
     const edit = request.body;
     GamesService.updateGamePlayer(edit.gameId, edit.playerId, edit.playerName)
         .then(game => response.send({result: game, error: ''}))
@@ -118,7 +131,7 @@ router.post<ParamsDictionary, WithError<IGame>, IGameEditPlayer>('/game-edit/pla
         });
 });
 
-router.delete<ParamsDictionary, WithError<IGame>, IGameRemovePlayer>('/game-edit/player', (request, response) => {
+router.delete<{}, WithError<IGame>, IGameRemovePlayer>('/game-edit/player', (request, response) => {
     const edit = request.body;
     GamesService.removeGamePlayer(edit.gameId, edit.playerId)
         .then(game => response.send({result: game, error: ''}))
@@ -128,7 +141,7 @@ router.delete<ParamsDictionary, WithError<IGame>, IGameRemovePlayer>('/game-edit
         });
 });
 
-router.post<ParamsDictionary, WithError<IGame>, IGameEditScore>('/game-edit/score', (request, response) => {
+router.post<{}, WithError<IGame>, IGameEditScore>('/game-edit/score', (request, response) => {
     const edit = request.body;
     GamesService.updateGameScore(edit.gameId, edit.playerId, edit.scoreId, edit.score)
         .then(game => response.send({result: game, error: ''}))
@@ -138,7 +151,7 @@ router.post<ParamsDictionary, WithError<IGame>, IGameEditScore>('/game-edit/scor
         });
 });
 
-router.delete<ParamsDictionary, WithError<IGame>, IGameRemoveScore>('/game-edit/score', (request, response) => {
+router.delete<{}, WithError<IGame>, IGameRemoveScore>('/game-edit/score', (request, response) => {
     const edit = request.body;
     GamesService.removeGameScore(edit.gameId, edit.playerId, edit.scoreId)
         .then(game => response.send({result: game, error: ''}))
