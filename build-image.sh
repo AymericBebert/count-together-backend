2096c6c5240e1835d79ec114e16df302ffe880d2
AymericBebert <aymeric.bernard@hotmail.fr>
2021-05-23 15:41:30 +0200

    Build script update

--- build-image.sh
+++ build-image.sh
@@ -2,23 +2,23 @@
 
 BUILD_CONFIGURATION=$1
 
-if [ -z $BUILD_CONFIGURATION ]; then
-  read -p "Enter build configuration [PRODUCTION/testing]: " bc
+if [ -z "$BUILD_CONFIGURATION" ]; then
+  read -rp "Enter build configuration [PRODUCTION/testing]: " bc
   BUILD_CONFIGURATION=${bc:-production}
 fi
 
 tags=$(git describe --contains)
 
-if [ -z $tags ]; then
+if [ -z "$tags" ]; then
   echo "No tag on current commit. Latest tags:"
   git tag | sort -V | tail -n 3
 
-  read -p "Enter new tag: " newtag
+  read -rp "Enter new tag: " newtag
 
   [ -z "$newtag" ] && echo "No version specified" && exit 1
 
-  if ! git tag $newtag; then
-    read -p "Git tag failed, continue? [y/N]: " c
+  if ! git tag "$newtag"; then
+    read -rp "Git tag failed, continue? [y/N]: " c
     if [[ ! $c =~ ^[Yy]$ ]]; then
       echo "Cancelled"
       exit 2
@@ -35,9 +35,9 @@ else
 fi
 
 function delete_new_tag() {
-  if [ -z $tags ]; then
+  if [ -z "$tags" ]; then
     echo "Removing new git tag $newtag"
-    git tag -d $newtag >/dev/null
+    git tag -d "$newtag" >/dev/null
   fi
 }
 
@@ -46,16 +46,16 @@ if [ "$BUILD_CONFIGURATION" != "production" ]; then
   version=$newtag-$BUILD_CONFIGURATION
 fi
 
-read -p "Will build version $version, configuration $BUILD_CONFIGURATION, continue? [y/N]: " c
+read -rp "Will build version $version, configuration $BUILD_CONFIGURATION, continue? [y/N]: " c
 if [[ ! $c =~ ^[Yy]$ ]]; then
   echo "Cancelled"
-    delete_new_tag
+  delete_new_tag
   exit 2
 fi
 
 echo "-----"
 echo "Building count-together-backend:$version..."
-docker build -t aymericbernard/count-together-backend:$version --build-arg BUILD_CONFIGURATION=$BUILD_CONFIGURATION --build-arg VERSION=$version . ||
+docker build -t "aymericbernard/count-together-backend:$version" --build-arg BUILD_CONFIGURATION="$BUILD_CONFIGURATION" --build-arg VERSION="$version" . ||
   {
     echo 'Build failed'
     delete_new_tag
@@ -63,11 +63,11 @@ docker build -t aymericbernard/count-together-backend:$version --build-arg BUILD
   }
 
 echo "Pushing count-together-backend:$version to docker registry..."
-docker push aymericbernard/count-together-backend:$version ||
+docker push "aymericbernard/count-together-backend:$version" ||
   {
     echo 'Push failed'
     exit 1
   }
 
 echo "Pushing git tag..."
-git push origin $version
+git push origin "$newtag"
