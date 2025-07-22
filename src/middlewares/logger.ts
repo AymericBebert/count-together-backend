1d302c106dcbee5b54dcf0a0a6f52fc1cd3e6a29
AymericBebert <aymeric.bernard@hotmail.fr>
2025-07-22 19:56:47 +0200

    chore: updates

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,10 +1,10 @@
 import express, {NextFunction} from 'express';
 
 export const loggerMiddleware = (request: express.Request, response: express.Response, next: NextFunction): void => {
-    const bodyStr = JSON.stringify(request.body);
+    const bodyStr = JSON.stringify(request.body || {});
     console.log(`http> ${request.method}`
         + ` - ${request.path}`
-        + ` - query ${JSON.stringify(request.query)}`
+        + ` - query ${JSON.stringify(request.query || {})}`
         + ` - body ${bodyStr.length > 2000 ? bodyStr.slice(0, 2000) + '[...]' : bodyStr}`);
     next();
 };
